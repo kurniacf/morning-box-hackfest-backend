272912c991e54e3fa34b15ea1ebdc220ea00662d
christhopermarcelino <christhopermarcelino@gmail.com>
2023-02-27 12:51:57 +0700

    chore: install godotenv

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
+	var port string
+
+	if os.Getenv("APP_ENV") == "production" {
+		port = fmt.Sprintf(":%s", os.Getenv("PORT"))
+	} else {
+		port = ":8080"
+	}
+
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
@@ -16,5 +35,5 @@ func main() {
 		})
 	})
 
-	r.Run()
+	r.Run(port)
 }
