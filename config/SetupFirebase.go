2fb32c7925051cfc0f3aa8a1c0fecfe50bd68ed3
christhopermarcelino <christhopermarcelino@gmail.com>
2023-02-27 19:35:32 +0700

    fix: wrong path to credentials

--- config/SetupFirebase.go
+++ config/SetupFirebase.go
@@ -10,20 +10,21 @@ import (
 )
 
 func SetupFirebase() *auth.Client {
-	serviceAccountKeyFilePath, err := filepath.Abs("../credentials.json")
+	serviceAccountKeyFilePath, err := filepath.Abs("./credentials.json")
+
 	if err != nil {
-		panic("Unable to load serviceAccountKeys.json file")
+		panic("Unable to load credentials")
 	}
 	opt := option.WithCredentialsFile(serviceAccountKeyFilePath)
 	//Firebase admin SDK initialization
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
-		panic("Firebase load error")
+		panic(err)
 	}
 	//Firebase Auth
 	auth, err := app.Auth(context.Background())
 	if err != nil {
-		panic("Firebase load error")
+		panic(err)
 	}
 	return auth
 }
