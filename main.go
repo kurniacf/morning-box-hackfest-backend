1625cfb45030cd3bd7997b0179b1cbffdef5a69d
christhopermarcelino <christhopermarcelino@gmail.com>
2023-02-27 12:39:08 +0700

    chore: install gin

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"message": "Hackfest 2023",
+		})
+	})
+
+	r.Run()
+}
