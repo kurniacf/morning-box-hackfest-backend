6a5fb1ccc58c917abc977d3b8ca0634e8ba6086b
christhopermarcelino <christhopermarcelino@gmail.com>
2023-03-04 15:10:38 +0700

    feat: add fetch to package foods

--- handler/PackageHandler.go
+++ handler/PackageHandler.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"morning-box-hackfest-be/config"
 	"morning-box-hackfest-be/model"
+	"morning-box-hackfest-be/repository"
 	"morning-box-hackfest-be/service"
 	"net/http"
 
@@ -17,11 +19,27 @@ func NewPackageHandler(service service.PackageService) *PackageHandler {
 }
 
 func (h *PackageHandler) GetAllPackages(c *gin.Context) {
+
 	packages, err := h.service.GetAllPackages()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get packages"})
 		return
 	}
+
+	firestoreClient := config.GetFirestoreClient()
+	foodRepo := repository.NewFoodRepository(firestoreClient)
+	foodService := service.NewFoodService(foodRepo)
+
+	for _, pack := range packages {
+		pack.Foods.Monday, _ = foodService.GetFood(pack.Foods.Monday.Id)
+		pack.Foods.Tuesday, _ = foodService.GetFood(pack.Foods.Tuesday.Id)
+		pack.Foods.Wednesday, _ = foodService.GetFood(pack.Foods.Wednesday.Id)
+		pack.Foods.Thursday, _ = foodService.GetFood(pack.Foods.Thursday.Id)
+		pack.Foods.Friday, _ = foodService.GetFood(pack.Foods.Friday.Id)
+		pack.Foods.Saturday, _ = foodService.GetFood(pack.Foods.Saturday.Id)
+		pack.Foods.Sunday, _ = foodService.GetFood(pack.Foods.Tuesday.Id)
+	}
+
 	c.JSON(http.StatusOK, packages)
 }
 
@@ -32,6 +50,19 @@ func (h *PackageHandler) GetPackage(c *gin.Context) {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Package not found"})
 		return
 	}
+
+	firestoreClient := config.GetFirestoreClient()
+	foodRepo := repository.NewFoodRepository(firestoreClient)
+	foodService := service.NewFoodService(foodRepo)
+
+	pkg.Foods.Monday, _ = foodService.GetFood(pkg.Foods.Monday.Id)
+	pkg.Foods.Tuesday, _ = foodService.GetFood(pkg.Foods.Tuesday.Id)
+	pkg.Foods.Wednesday, _ = foodService.GetFood(pkg.Foods.Wednesday.Id)
+	pkg.Foods.Thursday, _ = foodService.GetFood(pkg.Foods.Thursday.Id)
+	pkg.Foods.Friday, _ = foodService.GetFood(pkg.Foods.Friday.Id)
+	pkg.Foods.Saturday, _ = foodService.GetFood(pkg.Foods.Saturday.Id)
+	pkg.Foods.Sunday, _ = foodService.GetFood(pkg.Foods.Sunday.Id)
+
 	c.JSON(http.StatusOK, pkg)
 }
 
